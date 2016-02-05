5269c9e74931a78803f31d7320ad4703bf7fb6bc
Toni Gurski <tg@mandarin-medien.de>
2016-02-05 10:06:24 +0100

    updated test output RowContentNode.nodes : [ContentNode,ContentNode,ContentNode]

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -12,29 +12,32 @@ class DefaultController extends Controller
     public function indexAction()
     {
 
-        $node = new RowContentNode();
-        $node->setName("Parent Node");
-        $node->setAttributes(Array('data-custom-data'=>'foobar'));
+        $nodeRow = new RowContentNode();
+        $nodeRow->setName("Parent Node");
+        $nodeRow->setAttributes(Array('data-custom-data'=>'foobar'));
+
+
+/*        $nodeR1 = new RowContentNode();
+        $nodeR1->setName("Parent Node");
+        $nodeRow->addNode($nodeR1);*/
 
         $node1 = new ContentNode();
         $node1->setName("Child Node 1");
-        $node->addNode($node1);
+        $nodeRow->addNode($node1);
 
         $node2 = new ContentNode();
         $node2->setName("Child Node 2");
-        $node->addNode($node2);
+        $nodeRow->addNode($node2);
 
         $node3 = new ContentNode();
         $node3->setName("Child Node 3");
-        $node->addNode($node3);
+        $nodeRow->addNode($node3);
 
         return $this->render('MMCmfContentBundle:Default:index.html.twig',
             array(
-                'node' => $node
+                'node' => $nodeRow
             )
         );
-
-
 
     }
 }
