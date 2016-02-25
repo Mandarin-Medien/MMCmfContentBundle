86e35c604124c06a002f2f6f4aa464025166cef5
Toni Gurski <tg@mandarin-medien.de>
2016-02-25 11:24:22 +0100

    Implemented first Content Management functionalities like inline editing and sorting by drag and drop or controls

--- Resources/public/js/mmCmfContentEditor.js
+++ Resources/public/js/mmCmfContentEditor.js
@@ -1,16 +1,33 @@
+/**
+ * This jQuery plugin will add all GUI functionalities for the MMCmfContentBundle
+ */
 (function ($) {
 
-
+    /**
+     *
+     * @param element
+     * @param options
+     * @constructor
+     */
     var MmCmfContentEditor = function (element, options) {
 
+        this.element = $(element)
+        this.settings = options;
     };
 
+
+    /**
+     * Returns gui elemets which lets the user move the position of the current ContentNode
+     *
+     * @returns {*|HTMLElement}
+     */
     MmCmfContentEditor.prototype.generatePositionSwitchControls = function () {
 
-        /**
-         * man position switcher
-         */
+        var $contentNode = this.element;
 
+        /**
+         * manual position switcher
+         */
         var $posiswitch = $('<div class="posiswitch"><div class="upper"><i class="fa fa-chevron-up"></i></div><div class="downer"><i class="fa fa-chevron-down"></i></div></div>');
 
         /**
@@ -21,11 +38,8 @@
             e.preventDefault();
 
             var $ele = $(this);
-            var $contentNode = $ele.parents('.ContentNode-settings').parent();
 
-            console.log('posiswitch', $ele, $ele.hasClass('upper'));
             if ($ele.hasClass('upper')) {
-
                 $contentNode.prev().before($contentNode);
             }
 
@@ -37,12 +51,25 @@
         return $posiswitch;
     };
 
+    /**
+     * Returns Grid-System-Size selectbox which lets the user change the grid size based on the viewport
+     *
+     * @returns {*|HTMLElement}
+     */
     MmCmfContentEditor.prototype.generateColumnSelect = function ($gridSize, $gridCount) {
+
         var $select = $('<select />').data('grid-size', $gridSize);
+
+        $select.append('<option></option>');
 
         for (var i = 1; i <= $gridCount; i++) {
 
-            var $option = $('<option class="col-' + $gridSize + '-' + i + '">' + i + '</option>')
+            var $colClass = 'col-' + $gridSize + '-' + i;
+            var $option = $('<option value="' + $colClass + '">' + i + '</option>');
+
+            if (this.element.hasClass($colClass))
+                $option.attr('selected', 'selected');
+
             $select.append($option);
         }
 
@@ -54,6 +81,11 @@
 
     };
 
+    /**
+     * Returns the CMF-Settings DOM-Element which lets the user control some ContentNode attributes
+     *
+     * @returns {*|HTMLElement}
+     */
     MmCmfContentEditor.prototype.generateSettingsBox = function () {
 
         var $div = $('<div class="ContentNode-settings"><b class="ContentNode-settings-gear"><i class="fa fa-gear"></i></b><br></div>');
@@ -68,19 +100,45 @@
         return $div;
     };
 
-    MmCmfContentEditor.prototype.buildGUI = function($settings){
+    /**
+     * creates CmfContentNode settings gui
+     */
+    MmCmfContentEditor.prototype.buildGUI = function () {
 
+        var $settings = this.settings;
         var $boxInner = $('<div class="inner" />');
 
-        for(var i in $settings.gridSizes)
-        {
-            var $selectBox = this.generateColumnSelect($settings.gridSizes[i],$settings.gridCount);
+        for (var i in $settings.gridSizes) {
+            var $selectBox = this.generateColumnSelect($settings.gridSizes[i], $settings.gridCount);
             $boxInner.append($selectBox);
         }
 
+        /**
+         * bind change event
+         */
+        $boxInner.on('change', 'select', function (e) {
+
+            e.preventDefault();
+
+            $box.parent().removeClass(function (index, css) {
+                return (css.match(/col-[a-z1-9\-]*/g) || []).join(' ');
+            });
+
+            $boxInner.find('select').each(function () {
+                $box.parent().addClass($(this).val());
+            });
+        });
+
+
+        /**
+         * append position switch widget
+         */
         var $posSwitch = this.generatePositionSwitchControls();
         $boxInner.append($posSwitch);
 
+        /**
+         * append to inner box div
+         */
         var $box = this.generateSettingsBox();
         $box.append($boxInner);
 
@@ -88,7 +146,68 @@
 
     };
 
+    /**
+     * looks for data-cmf-field DOM-Elements and binds contenteditable functionality
+     * @param $elements
+     */
+    function initiateContenteditable($elements) {
 
+        var $dataFields = $($elements).find('[data-cmf-field]');
+
+        /**
+         * bin editable transformation
+         */
+        $dataFields.on('dblclick', function () {
+            $(this).attr('contenteditable', 'true').focus();
+        }).on('blur', function () {
+            $(this).attr('contenteditable', 'false');
+        });
+    }
+
+
+    /**
+     * Load Parents of given ContentNodes and initiates Dragula on it
+     *
+     * @param $elements
+     */
+    function initiateDragula($elements) {
+        var $draggableContainers = new Array();
+
+        $elements.parent().each(function () {
+            if ($(this).attr('class').indexOf('ContentNode') != -1)
+                $draggableContainers.push(this);
+
+        });
+
+        console.log($elements.parent(), $draggableContainers);
+
+        var $draguala = dragula($draggableContainers, {
+
+            accepts: function (el, target, source, sibling) {
+
+                if (!($(el).hasClass('ContentNode') || $(el).hasClass('ParagraphContentNode')))
+                    return false;
+
+                return true; // elements can be dropped in any of the `containers` by default
+            }
+        });
+
+
+        $draguala.on('drag', function (el, source) {
+            $(el).css('background-color', 'rgba( 255 , 0 , 255 ,.5)');
+        });
+
+        $draguala.on('dragend', function (el) {
+            $(el).css('background-color', 'transparent');
+        });
+
+    }
+
+    /**
+     * bootstrap jquery plugin
+     *
+     * @param options
+     */
     $.fn.mmCmfContentEditor = function (options) {
 
         // Establish our default settings
@@ -100,12 +219,15 @@
 
         this.each(function () {
 
-            var $MmCmfContentEditor = new MmCmfContentEditor();
+            var $MmCmfContentEditor = new MmCmfContentEditor(this, settings);
             var $frontendEditWidget = $MmCmfContentEditor.buildGUI(settings);
 
             $(this).append($frontendEditWidget);
 
         });
-    }
+
+        initiateContenteditable(this);
+        initiateDragula(this);
+    };
 
 }(jQuery));
