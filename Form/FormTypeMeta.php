f0f13d1ce0459d91763b5795c8b00e383c694058
Christof Hufnagel <ch@mandarin-medien.de>
2017-05-23 16:42:23 +0200

    Extended FormTypeMeta Annotation with Options

--- Form/FormTypeMeta.php
+++ Form/FormTypeMeta.php
@@ -15,8 +15,47 @@ use Doctrine\Common\Annotations\Annotation;
 class FormTypeMeta extends Annotation
 {
 
+
+    /**
+     * @var string className of FormType
+     */
+    public $class;
+
+
+    /**
+     * @var array
+     */
+    public $options = [];
+
+
     public function getValue()
     {
-        return $this->value;
+        if($this->value) {
+            return $this->value;
+        } else {
+            return $this->class;
+        }
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getClass()
+    {
+        if($this->class) {
+            return $this->class;
+        } else {
+            return $this->value;
+        }
+    }
+
+
+    /**
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->options;
     }
 }
--- Form/FormTypeMetaReader.php
+++ Form/FormTypeMetaReader.php
@@ -6,6 +6,12 @@ use Doctrine\Common\Annotations\AnnotationReader;
 
 class FormTypeMetaReader
 {
+
+    /**
+     * @param $class
+     * @param $property
+     * @return null
+     */
     public function get($class, $property)
     {
 
@@ -19,5 +25,35 @@ class FormTypeMetaReader
         }
 
         return null;
+    }
+
+    public function getFormType($class, $property)
+    {
+        return $this->get($class, $property);
+    }
+
+    public function getOptions($class, $property)
+    {
+        $reader     = new AnnotationReader();
+        $property   = new \ReflectionProperty($class, $property);
+
+        $annotation = $reader->getPropertyAnnotation($property, FormTypeMeta::class);
+
+        if($annotation) {
+            return $annotation->getOptions();
+        } else {
+            return [];
+        }
+    }
+
+    /**
+     * @return FormTypeMeta
+     */
+    public function getFormTypeMeta($class, $property)
+    {
+        $reader     = new AnnotationReader();
+        $property   = new \ReflectionProperty($class, $property);
+
+        return $reader->getPropertyAnnotation($property, FormTypeMeta::class);
     }
 }
